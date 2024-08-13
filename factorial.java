d64d7332995c75389b965a5668028c4095e35056
summit28 <sumitraskar15@gmail.com>
2024-08-13 21:39:50 +0530

    finding factorial of any number

---
+++ factorial.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class factorial {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;//to hold number
+        int fact = 1;//to hold
+
+        System.out.print("Enter any positive number : ");
+        num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            fact *= i; //fact = 1 * 1 = 1 similar fact = 1 * 2 continue to num
+        }
+        System.out.println("Factoral:" + fact);
+        sc.close();
+    }
+}
