cb26e115f1bd43c4b6fd5eb7b470d8b9b30c7ec9
summit28 <sumitraskar15@gmail.com>
2024-08-17 21:31:45 +0530

    parameters vs arguments

--- functionbasic.java
+++ functionbasic.java
@@ -6,7 +6,7 @@ public class functionbasic {
         System.out.println("Hello World!");
     }
 
-    public static int calculatesum(int num1, int num2) {
+    public static int calculatesum(int num1, int num2) { // parameters or formal parameters 
         int sum = num1 + num2;
         return sum;
     }
@@ -16,7 +16,7 @@ public class functionbasic {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int sum = calculatesum(a, b); //function call
+        int sum = calculatesum(a, b); //function call // arguments or actual parameters
         System.out.println("sum is : " + sum);
         sc.close();
     }
