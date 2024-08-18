70de703057e1496998c8e95fd0f3fe2528222dcb
summit28 <sumitraskar15@gmail.com>
2024-08-18 16:56:35 +0530

    swaping-call by value

--- functionbasic.java
+++ functionbasic.java
@@ -11,6 +11,14 @@ public class functionbasic {
         return sum;
     }
 
+    public static void swap(int c, int d) {
+        int temp = c;
+        c = d;
+        d = temp;
+        System.out.println("c is " + c);
+        System.out.println("d is " + d);
+    }
+
     public static void main(String[] args) {
         printhelloworld(); //function call
         Scanner sc = new Scanner(System.in);
@@ -18,6 +26,14 @@ public class functionbasic {
         int b = sc.nextInt();
         int sum = calculatesum(a, b); //function call // arguments or actual parameters
         System.out.println("sum is : " + sum);
+
+        //swaping - value exchange
+        int c = 2;
+        int d = 4;
+        swap(c, d);
+        // System.out.println("c is "+ c);
+        // System.out.println("d is "+ d);
+
         sc.close();
     }
 }
