18e2e217eeee3aff94dd045d45b0d16a880808c2
summit28 <sumitraskar15@gmail.com>
2024-08-20 16:48:49 +0530

    find square root using inbuilt method in java

---
+++ functionbasic1.java
@@ -0,0 +1,9 @@
+
+public class functionbasic1 {
+
+    public static void main(String[] args) {
+        //find square root using inbuilt method in java
+        int a = 12;
+        System.out.println("square of a is : " + Math.sqrt(a));
+    }
+}
